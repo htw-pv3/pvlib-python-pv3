a52b96f17f21306cccb716ee0dcea5d28fbaaddb
mosc5 <moritz.schiel@gmx.net>
2021-06-30 18:36:48 +0200

    change power inputs #32

--- component_import.py
+++ component_import.py
@@ -2,15 +2,38 @@ import pvlib
 import numpy as np
 
 
-sma_sb_data = {
-    "ac_power": np.array([752.9, 2289.6, 3000]),  # eta25=.956, eta75=.96, eta100=.952
-    "dc_power": np.array([787.5, 2385, 3150]),  # Annahme: Strom bleibt in allen Punkten gleich
-    "dc_voltage": np.array([175., 530, 700]),
-    "dc_voltage_level": np.array(["Vmin", "Vnom", "Vmax"]),
-    "p_ac_0": 3150.,
-    "p_nt": 1.
-}
+def get_sma_sb_3000hf():
+    eta_min = [0, 0.945, 0.95, 0.947, 0.942, 0.931]  # P/P_max = 0, 0.2, 0.3, 0.5, 0.75, 1; U = 210V
+    eta_nom = [0, 0.953, 0.962, 0.963, 0.96, 0.952]  # P/P_max = 0, 0.2, 0.3, 0.5, 0.75, 1; U = 530V
+    eta_max = [0, 0.95, 0.96, 0.962, 0.961, 0.9524]  # P/P_max = 0, 0.2, 0.3, 0.5, 0.75, 1; U = 560V
+    p_dc_nom = 3150
+    p_dc = [0, 0.2 * p_dc_nom, 0.3 * p_dc_nom, 0.5 * p_dc_nom, 0.75 * p_dc_nom, 1 * p_dc_nom]
+    p_ac_min = []
+    p_ac_nom = []
+    p_ac_max = []
+    val_count = len(eta_min)
 
-sma_sb_3000_hf = pvlib.inverter.fit_sandia(sma_sb_data["ac_power"], sma_sb_data["dc_power"], sma_sb_data["dc_voltage"],
-                                           sma_sb_data["dc_voltage_level"], sma_sb_data["p_ac_0"], sma_sb_data["p_nt"])
-print(sma_sb_3000_hf)
+    for i in range(val_count):
+        p_ac_min.append(p_dc[i] * eta_min[i])
+        p_ac_nom.append(p_dc[i] * eta_nom[i])
+        p_ac_max.append(p_dc[i] * eta_max[i])
+
+    ac_power = np.array(p_ac_min + p_ac_nom + p_ac_max)
+
+    sma_sb_data = {
+        "ac_power": ac_power,
+        "dc_power": np.array(p_dc + p_dc + p_dc),  # Annahme: Strom bleibt in allen Punkten gleich
+        "dc_voltage": np.array([175.] * val_count + [530.] * val_count + [560.] * val_count),
+        "dc_voltage_level": np.array(["Vmin"] * val_count + ["Vnom"] * val_count + ["Vmax"] * val_count),
+        "p_ac_0": 3000.,
+        "p_nt": 1.
+    }
+
+    sma_sb_3000_hf = pvlib.inverter.fit_sandia(sma_sb_data["ac_power"], sma_sb_data["dc_power"], sma_sb_data["dc_voltage"],
+                                               sma_sb_data["dc_voltage_level"], sma_sb_data["p_ac_0"], sma_sb_data["p_nt"])
+    return sma_sb_3000_hf
+
+
+if __name__ == "__main__":
+    inv = get_sma_sb_3000hf()
+    print(inv)
